bb16925bd6fd41f73b43e308928dfbe5da461668
POI <POI>
2023-07-20 16:13:10 +0900

    余力取得処理作成

--- src/kabusapi/wallet.py
+++ src/kabusapi/wallet.py
@@ -6,12 +6,62 @@ class Wallet():
         self.api_headers = api_headers
 
     def cash(self):
-        '''現物の余力を取得する'''
-        pass
+        '''
+        現物の余力を取得する
 
-    def margin(self):
-        '''信用の余力を取得する'''
-        pass
+        Returns:
+            response.content(dict): 現物余力情報
+                StockAccountWallet(float): 現物買付可能額
+                AuKCStockAccountWallet(float): auカブコム証券可能額
+                AuJbnStockAccountWallet(float): auじぶん銀行残高
+
+        '''
+        url = f'http://localhost:18080/kabusapi/wallet/cash/'
+
+        try:
+            response = requests.get(url, headers = self.api_headers)
+        except Exception as e:
+            pass # TODO ここにエラー処理
+            return False
+
+        if response.status_code != 200:
+            pass # TODO ここにエラー処理
+            return False
+
+        return response.content
+
+    def margin(self, stock_code = None, market_code = None):
+        '''
+        信用の余力を取得する
+
+        Args:
+            stock_code(int or str): 証券コード [任意]
+            market_code(int or str): 市場コード [任意]
+                1: 東証、3: 名証
+
+        Returns:
+            response.content(dict): 信用余力情報
+                MarginAccountWallet(float): 信用新規可能額
+                DepositkeepRate(float): 保証金維持率 ※銘柄指定時のみ
+                ConsignmentDepositRate(float): 委託保証金率 ※銘柄指定時のみ
+                CashOfConsignmentDepositRate(float): 現金委託保証金率 ※銘柄指定時のみ
+        '''
+        url = f'http://localhost:18080/kabusapi/wallet/margin/'
+
+        if stock_code != None and market_code != None:
+            url += f'{stock_code}@{market_code}'
+
+        try:
+            response = requests.get(url, headers = self.api_headers)
+        except Exception as e:
+            pass # TODO ここにエラー処理
+            return False
+
+        if response.status_code != 200:
+            pass # TODO ここにエラー処理
+            return False
+
+        return response.content
 
     def future(self):
         '''先物の余力を取得する'''
