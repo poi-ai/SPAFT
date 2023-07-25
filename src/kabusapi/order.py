17e28feaa39da8cb018827750ff950bd5c9fd514
POI <POI>
2023-07-25 09:34:37 +0900

    発注APIの引数説明追加

--- src/kabusapi/order.py
+++ src/kabusapi/order.py
@@ -15,7 +15,48 @@ class Order():
         日本株の注文を発注する
 
         Args:
-            TODO
+            password(str): 注文パスワード [必須]
+            stock_code(str): 証券コード [必須]
+            exchange(int): 市場コード [必須]
+                1: 東証、3: 名証、5: 福証、6: 札証
+            side(str): 売買区分 [必須]
+                1: 売、2: 買
+            cash_margin(int): 信用区分 [必須]
+                1: 現物、2: 新規、3: 返済
+            deliv_type(int): 受渡区分 [必須]
+                0: 指定なし、1: 自動振替、2: お預り金、3: auマネーコネクト
+            account_type(int): 口座種別 [必須]
+                2: 一般、4: 特定、12: 法人
+            qty(int): 注文株数
+            front_order_type(int): 執行条件 [必須]
+                10: 成行、13: 寄成(前場)、14: 寄成(後場)、15: 引成(前場)、16: 引成(後場)、17: IOC成行、
+                20: 指値、21: 寄指(前場)、22: 寄指(後場)、23: 引指(前場)、24: 引指(後場)、25: 不成(前場)、
+                26: 不成(後場)、27: IOC指値、30: 逆指値
+            price(int): 注文価格 [必須]
+                ※指値でない場合は0を指定
+            expire_day(int、yyyyMMdd): 注文有効期限 [必須]
+            margin_trade_type(int): 信用取引区分 [任意]
+                1: 制度信用、2: 一般信用(長期)、3: 一般信用(デイトレ)
+            margin_premium_unit(float): プレミアム料 [任意]
+            fund_type(str): 資産区分 [任意] ※現物注文は必須
+                '  '(半ｽﾍﾟｰｽ2文字): 現物売の場合、02: 保護、AA: 信用代用、11: 信用取引
+            close_position_order(int): 決済順序 [任意] ※信用返済の場合必須
+                0: 日付(古い順)、損益(高い順)、1: 日付(古い順)、損益(低い順)、2: 日付(新しい順)、損益(高い順)、
+                3: 日付(新しい順)、損益(低い順)、4: 損益(高い順)、日付(古い順)、5: 損益(高い順)、日付(新しい順)、
+                6: 損益(低い順)、日付(古い順)、7: 損益(低い順)、日付(新しい順)
+            close_positions(array[dict, dict] or dict): 信用返済 [任意] ※信用返済の場合必須
+                HoldID(str): 返済建玉ID [任意]
+                Qty(int): 返済建玉数量 [任意]
+            reverse_limit_order(dict): 逆指値条件 [任意]
+                TriggerSec(int): トリガー銘柄種別 [必須]
+                    1: 発注銘柄の株価、2: 日経平均株価、3: TOPIX指数
+                TriggerPrice(float): トリガー価格 [必須]
+                UnderOver(int): トリガー条件 [必須]
+                    1: 以下、2: 以上
+                AfterHitOrderType(int): トリガー発動時執行条件 [必須]
+                    1: 成行、2: 指値、3: 不成
+                AfterHitPrice(float): トリガー発動時注文価格 [必須]
+                    ※成行の場合は0
 
         Returns:
             response.content(dict): 注文結果情報
